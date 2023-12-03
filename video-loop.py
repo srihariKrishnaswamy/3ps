3782efa918be3501ffcde9f5d50b0e7b8dda6f4f
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-12-03 12:42:07 -0800

    very very very rought ptype

--- video-loop.py
+++ video-loop.py
@@ -5,8 +5,8 @@ import time
 focal_length = 50 # my mac has a 50mm focal length
 capture = cv2.VideoCapture(0)
 frame_height, frame_width = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-object_real_width = 19
-object_real_height = 25
+object_real_width = 19 #cm
+object_real_height = 25 #cm
 
 if not capture.isOpened():
     print("Error: Could not open camera.")
@@ -14,13 +14,17 @@ if not capture.isOpened():
 
 cv2.namedWindow('Camera Feed', cv2.WINDOW_NORMAL)
 
-def get_distance_from_camera_rectangular(cnt, image_size, real_width, real_height):
+def get_distance_from_camera_rectangular(frame, cnt, image_size, real_width, real_height):
     x, y, w, h = cv2.boundingRect(cnt)
     apparent_width = w
     apparent_height = h
     average_apparent_size = (apparent_width + apparent_height) / 2.0
     distance = (real_width * image_size) / (2 * average_apparent_size * focal_length * np.tan(np.radians(39.6 / 2)))
-    return distance
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    distance_text = f"Distance: {distance:.2f} cm"
+    cv2.putText(frame, distance_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+
+    return frame
 
 def outlineRect(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -28,7 +32,7 @@ def outlineRect(frame):
     edges = cv2.Canny(blurred, 50, 150)
     # dialations
     kernel = np.ones((7, 7), np.uint8)
-    edges = cv2.dilate(edges, kernel, iterations=1)
+    edges = cv2.dilate(edges, kernel, iterations=2)
     
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -40,9 +44,8 @@ def outlineRect(frame):
         cv2.drawContours(mask, [largest_contour], -1, (255), thickness=cv2.FILLED)
         result_frame = cv2.bitwise_and(frame, frame, mask=mask)
         cnt_area = cv2.contourArea(largest_contour)
-        est_distance = get_distance_from_camera_rectangular(largest_contour, frame_width, object_real_width, object_real_height)
-        print("CONTOUR AREA: " + str(cnt_area))
-        print("EST DISTANCE: " + str(est_distance))
+        result_frame = get_distance_from_camera_rectangular(result_frame, largest_contour, frame_width, object_real_width, object_real_height)
+        # print("CONTOUR AREA: " + str(cnt_area))
         
         return result_frame
 
