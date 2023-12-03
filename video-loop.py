1c5efded1eaf729114642eaa391fa944349e3d15
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-12-03 12:57:36 -0800

    more calibration

--- video-loop.py
+++ video-loop.py
@@ -2,11 +2,11 @@ import cv2
 import numpy as np
 import time
 
-focal_length = 50 # my mac has a 50mm focal length
+focal_length = .50 # my mac has a 50mm focal length
 capture = cv2.VideoCapture(0)
 frame_height, frame_width = int(capture.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(capture.get(cv2.CAP_PROP_FRAME_WIDTH))
-object_real_width = 19 #cm
-object_real_height = 25 #cm
+object_real_width = 10 #cm
+object_real_height = 10 #cm
 
 if not capture.isOpened():
     print("Error: Could not open camera.")
@@ -19,11 +19,11 @@ def get_distance_from_camera_rectangular(frame, cnt, image_size, real_width, rea
     apparent_width = w
     apparent_height = h
     average_apparent_size = (apparent_width + apparent_height) / 2.0
-    distance = (real_width * image_size) / (2 * average_apparent_size * focal_length * np.tan(np.radians(39.6 / 2)))
+    distance = (real_width * image_size) / (2 * average_apparent_size * focal_length)
     cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
     distance_text = f"Distance: {distance:.2f} cm"
     cv2.putText(frame, distance_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
-
+    print(distance_text)
     return frame
 
 def outlineRect(frame):
@@ -31,13 +31,10 @@ def outlineRect(frame):
     blurred = cv2.GaussianBlur(gray, (15, 15), 0)
     edges = cv2.Canny(blurred, 50, 150)
     # dialations
-    kernel = np.ones((7, 7), np.uint8)
-    edges = cv2.dilate(edges, kernel, iterations=2)
-    
+    kernel = np.ones((15, 15), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=1)
     contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
     contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 1000]
-
     if contours:
         largest_contour = max(contours, key=cv2.contourArea)
         mask = np.zeros_like(edges)
@@ -46,9 +43,7 @@ def outlineRect(frame):
         cnt_area = cv2.contourArea(largest_contour)
         result_frame = get_distance_from_camera_rectangular(result_frame, largest_contour, frame_width, object_real_width, object_real_height)
         # print("CONTOUR AREA: " + str(cnt_area))
-        
         return result_frame
-
     return edges
 
 while True:
