f451ba714c03f1bf1a00bcabcb3c055e3f3557ca
Srihari Krishnaswamy <sriharik844@gmail.com>
2023-12-27 12:44:34 -0800

    all the stuffs

--- video-loop.py
+++ video-loop.py
@@ -14,6 +14,52 @@ if not capture.isOpened():
 
 cv2.namedWindow('Camera Feed', cv2.WINDOW_NORMAL)
 
+
+
+def outlineHough(frame):
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    blurred = cv2.GaussianBlur(gray, (15, 15), 0)
+    edges = cv2.Canny(blurred, 50, 150)
+    # dialations
+    kernel = np.ones((15, 15), np.uint8)
+    edges = cv2.dilate(edges, kernel, iterations=1)
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = [cnt for cnt in contours if cv2.contourArea(cnt) > 1000]
+    if contours:
+        cnt = max(contours, key=cv2.contourArea)
+        frame = get_distance_from_camera_hough(frame, cnt)
+    return frame
+
+def get_distance_from_camera_edge(frame, cnt, image_size, real_width, real_height):
+    x, y, w, h = cv2.boundingRect(cnt)
+    cnt_area = cv2.contourArea(cnt)
+    distance = (((real_width + real_height) / 2) * image_size) / (cnt_area * focal_length) # make this take width and height into account (this is really sketch as is)
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+    distance_text = f"Distance: {distance:.2f} cm"
+    cv2.putText(frame, distance_text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
+    print(distance_text)
+    return frame
+
+def get_distance_from_camera_hough(frame, cnt):
+    mask = np.zeros_like(frame)
+    cv2.drawContours(mask, [cnt], -1, (255), thickness=cv2.FILLED)
+    gray_mask = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray_mask, 50, 150)
+    lines = cv2.HoughLines(edges, 1, np.pi / 180, threshold=100)
+    if lines is not None:
+        for line in lines:
+            rho, theta = line[0]
+            a = np.cos(theta)
+            b = np.sin(theta)
+            x0 = a * rho
+            y0 = b * rho
+            x1 = int(x0 + 1000 * (-b))
+            y1 = int(y0 + 1000 * (a))
+            x2 = int(x0 - 1000 * (-b))
+            y2 = int(y0 - 1000 * (a))
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    return frame
+
 def get_distance_from_camera_rectangular(frame, cnt, image_size, real_width, real_height):
     x, y, w, h = cv2.boundingRect(cnt)
     apparent_width = w
@@ -26,9 +72,10 @@ def get_distance_from_camera_rectangular(frame, cnt, image_size, real_width, rea
     print(distance_text)
     return frame
 
+
 def outlineRect(frame):
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    blurred = cv2.GaussianBlur(gray, (15, 15), 0)
+    blurred = cv2.GaussianBlur(gray, (5, 5), 0)
     edges = cv2.Canny(blurred, 50, 150)
     # dialations
     kernel = np.ones((15, 15), np.uint8)
@@ -42,8 +89,9 @@ def outlineRect(frame):
         mask = np.zeros_like(edges)
         cv2.drawContours(mask, [largest_contour], -1, (255), thickness=cv2.FILLED)
         result_frame = cv2.bitwise_and(frame, frame, mask=mask)
-        cnt_area = cv2.contourArea(largest_contour)
-        result_frame = get_distance_from_camera_rectangular(result_frame, largest_contour, frame_width, object_real_width, object_real_height)
+        result_frame = get_distance_from_camera_edge(result_frame, largest_contour, frame_width, object_real_width, object_real_height)
+
+        # result_frame = get_distance_from_camera_rectangular(result_frame, largest_contour, frame_width, object_real_width, object_real_height)
         # print("CONTOUR AREA: " + str(cnt_area))
         return result_frame
     return edges
@@ -55,7 +103,7 @@ while True:
         print("Error: Could not read frame.")
         break
 
-    frame = outlineRect(frame)
+    frame = outlineHough(frame)
     cv2.imshow('Altered', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
