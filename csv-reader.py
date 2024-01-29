32abea0badb0ef64d43f0de2af7e4c3626b5a7ff
Deleted-0970 <derekliuj@gmail.com>
2024-01-28 20:24:52 -0800

    creates silly graphs now with csvs

--- csv-reader.py
+++ csv-reader.py
@@ -1,1 +1,17 @@
-import matplotlib
+import matplotlib.pyplot as plt # for plotting
+import numpy as np # doin math ? prolly won't use this
+import pandas as pd # reading csv
+
+# read csv file. 
+# check how the csv file is formatted
+data = pd.read_csv('testData.csv')
+
+# GROUP BY receivers
+groups = data['Reciever'].unique()
+group_data = {receiver: data[data['Reciever'] == receiver] for receiver in groups}
+
+# plot each receiver with receiver data
+for receiver, receiver_data in group_data.items():
+    plt.plot(receiver_data['Day'], receiver_data['# of sturgeon'])
+
+plt.show()
